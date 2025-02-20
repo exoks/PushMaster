4ecaf430ab5d350193cbf8d0f276bc7637e45f7d
Oussama Ezzaou <oezzaou@c2r1p9.1337.ma>
2025-02-20 10:53:47 +0100

    run.sh: add safe guard for generator

--- my_tester/run.sh
+++ my_tester/run.sh
@@ -5,7 +5,7 @@
 #  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
 #  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
 #  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/02/19 15:42:39 by oezzaou
-#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/02/19 20:50:51 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/02/20 10:50:59 by oezzaou
 #  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
 #  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
 #  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
@@ -17,12 +17,15 @@
 #====<[ Variables: ]>===========================================================
 gen_src="generator.c"
 gen_exec="generator"
+push_swap_path=../push_swap
 
 #====<[ Colors: ]>==============================================================
 GREEN="\033[1;32m"
 CYAN="\033[1;36m"
 PINK="\033[0;38;5;199m"
+YELLOW="\033[0;93m"
 RED="\033[1;31m"
+GRAY="\033[0;90m"
 RESET="\033[1;0m"
 
 #====<[ Script: ]>==============================================================
@@ -33,41 +36,41 @@ then
 fi
 
 if [ ! -f generator ]; then
-  cc  generator.c -o generator;
-  printf "${GREEN}[OK]${RESET}: ${PINK}Compiling${RESET} %-29s| ${gen_exec}\n" "${gen_src}";
-else
-	echo "${GREEN}[OK]: ${CYAN}${gen_exec} ✔️${RESET}"
+  cc ${gen_src} -o ${gen_exec};
+  if [ $? -ne 0 ]; then
+    echo "${YELLOW}run.sh${RESET}: ${RED}compiling error${RESET} > ${gen_src}"
+    exit 1;
+  fi
+  printf "${GREEN}[OK]${RESET}: ${PINK}Compiling${RESET} %-22s| ${gen_exec}\n"\
+         "${gen_src}";
 fi
+echo "${GREEN}[OK]: ${CYAN}${gen_exec} ✔️${RESET}";
 
-# if [! -f generator]
-#
-# generator $1 $2 $3 > file
-#
-# data=$(cat file)
-#
-# ./../push_swap $data
-#
-# echo -ne "\033[1;32m$data\033[1;0m\n"
-#
-# all=`./../push_swap $data`
-# nbr=`echo "$all" | wc -l`
-# ra=`echo "$all" | awk '/^ra$/' | wc -l`
-# rb=`echo "$all" | awk '/^rb$/' | wc -l`
-# rr=`echo "$all" | awk '/^rr$/' | wc -l`
-# pb=`echo "$all" | awk '/^pb$/' | wc -l`
-# pa=`echo "$all" | awk '/^pa$/' | wc -l`
-# sb=`echo "$all" | awk '/^sb$/' | wc -l`
-# rra=`echo "$all" | awk '/^rra$/' | wc -l`
-# rrb=`echo "$all" | awk '/^rrb$/' | wc -l`
-# rrr=`echo "$all" | awk '/^rrr$/' | wc -l`
-#
-# echo -ne "\n\033[1;36m=>\033[1;0m waiting "
-# i=0
-# while [ $i -lt 3 ]; do
-# 	echo -n "."
-# 	sleep 1
-# 	i=$(($i + 1))
-# done
+#===
+rand_nbr=`./${gen_exec} $1 $2 $3`
+
+echo "${GREEN}${rand_nbr}${RESET}"
+
+cmds=`./${push_swap_path} ${rand_nbr}`
+ra=`echo  "${cmds}" | awk '/^ra$/'  | wc -l`
+rb=`echo  "${cmds}" | awk '/^rb$/'  | wc -l`
+rr=`echo  "${cmds}" | awk '/^rr$/'  | wc -l`
+pb=`echo  "${cmds}" | awk '/^pb$/'  | wc -l`
+pa=`echo  "${cmds}" | awk '/^pa$/'  | wc -l`
+sb=`echo  "${cmds}" | awk '/^sb$/'  | wc -l`
+rra=`echo "${cmds}" | awk '/^rra$/' | wc -l`
+rrb=`echo "${cmds}" | awk '/^rrb$/' | wc -l`
+rrr=`echo "${cmds}" | awk '/^rrr$/' | wc -l`
+ctr=`echo "${cmds}" | wc -l`
+
+printf "${RED}root${RESET}@${GREEN}oezzaou${RESET}❯${GRAY} waiting ${RESET}"
+for second in {1..3}; do 
+  printf "."; sleep 1;
+  if [ ${second} -eq 3 ]; then
+    printf "\n";
+  fi
+done
+
 # echo -ne "\n"
 #
 # if [ ${nbr} -gt 1500 ]
