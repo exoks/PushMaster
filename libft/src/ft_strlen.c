ae5960b208d7e34ef9d2c81709ee744c3d2656de
Oussama Ezzaou <oussamaezzaou@gmail.com>
2025-03-01 15:40:06 +0000

    push_swap: libft: ft_strlen norm is done

--- libft/src/ft_strlen.c
+++ libft/src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: oezzaou <oezzaou@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 21:16:13 by oezzaou           #+#    #+#             */
-/*   Updated: 2022/10/05 13:43:12 by oezzaou          ###   ########.fr       */
+/*   Updated: 2025/03/01 15:39:26 by oezzaou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,12 +14,12 @@
 //====<[ ft_strlen: ]>==========================================================
 size_t	ft_strlen(const char *s)
 {
-  int len;
+	int	len;
 
-  len = -1;
-  while (s[++len] != '\0')
-    ;
-  return (len);
+	len = -1;
+	while (s[++len] != '\0')
+		;
+	return (len);
 }
 // size_t	ft_strlen(const char *s)
 // {
